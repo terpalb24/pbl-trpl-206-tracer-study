7ee8ffbb4e112c0a6f167b4ea5cc4b57a3cf59b1
hasnadhilaa <hasnadila0@gmail.com>
2025-04-21 20:12:43 +0700

    Login-Page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('test');
-});
-Route::get('/halo', function(){
-    return view('halo');
-});
+
+// Menampilkan halaman login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
