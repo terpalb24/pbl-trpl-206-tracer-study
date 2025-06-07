96943dc6e1ab81171ea515313047977628f3f3f2
andriputra-ds <andriputra110906@gmail.com>
2025-06-08 00:26:37 +0700

    menghapus maatexcel g guna

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col items-center justify-between p-4">
-    <img src="{{ asset('assets/images/Group 3.png') }}" alt="Tracer Study Polibatam Logo" class="w-36 mt-2 object-contain">
+    <img src="{{ asset('assets/images/Group 3.png') }}" alt="Tracer Study Polibatam Logo" class=" h-36 mt-2 object-contain">
     <button id="close-sidebar" class="text-white text-xl lg:hidden focus:outline-none absolute top-4 right-4">
         <i class="fas fa-times"></i>
     </button>
