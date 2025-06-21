df4701ca430146617a5a5a5a98e0a6eaa52ebd11
hasnadhilaa <hasnadila0@gmail.com>
2025-06-22 05:32:47 +0700

    fixed sidebar admin

--- resources/views/components/layout-admin.blade.php
+++ resources/views/components/layout-admin.blade.php
@@ -1,12 +1,21 @@
-<div class="flex min-h-screen w-full bg-gray-100 overflow-hidden" id="dashboard-container">
-    <!-- Sidebar -->
-    <aside {{ $attributes->merge(['class' => 'sidebar-menu w-64 bg-blue-950 text-white flex flex-col transition-all duration-300', 'id' => 'sidebar']) }}>
+<div class="w-full min-h-screen bg-gray-100 overflow-hidden" id="dashboard-container">
+    <!-- Fixed Sidebar -->
+    <aside
+        id="sidebar"
+        class="fixed top-0 left-0 w-64 h-screen bg-blue-950 text-white flex flex-col overflow-y-auto z-50 transition-transform duration-300
+            -translate-x-full lg:translate-x-0"
+        style="will-change: transform;"
+    >
         {{ $sidebar }}
     </aside>
 
-    <!-- Main Content -->
-    <main class="flex-grow overflow-y-auto" id="main-content">
+    <!-- Fixed Header -->
+    <header class="fixed top-0 left-0 right-0 z-40 lg:ml-64" style="height:80px;">
         {{ $header }}
+    </header>
+
+    <!-- Main Content -->
+    <main class="flex-grow overflow-y-auto transition-all duration-300 lg:ml-64 pt-20" id="main-content">
         {{ $slot }}
     </main>
 </div>
